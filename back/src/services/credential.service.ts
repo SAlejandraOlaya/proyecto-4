53ca45ab692bab820640e5618a8f4f5e06f197bd
Alejandra Olaya <salejandraolaya@gmail.com>
2024-09-25 16:00:36 -0500

    se reinstalan dependencias

--- back/src/services/credential.service.ts
+++ back/src/services/credential.service.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcryptjs from "bcryptjs";
 import CreateCredentialDto from "../dtos/createCredentialDto";
 import { CredentialRepository } from "../repositories/credential.repository";
 import { Credential } from "../entities/Credential";
@@ -6,12 +6,12 @@ import { Credential } from "../entities/Credential";
 
 export const createCredentialService = async (credentialDto: CreateCredentialDto): Promise<Credential> => {
     const { password } = credentialDto;
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcryptjs.hash(password, 10);
     const credential = CredentialRepository.create({ password: hashedPassword });
     await CredentialRepository.save(credential);
     return credential;
 }
 
 export const checkPasswordService = async (password: string, hashedPassword: string): Promise<boolean> => {
-    return bcrypt.compare(password, hashedPassword);
+    return bcryptjs.compare(password, hashedPassword);
 }
